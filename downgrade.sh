0f8faf3549fa171ad5f7451abbda60dc42468cce
Flynn Sharwood <69000191+flynnsharwood@users.noreply.github.com>
2023-10-14 17:27:57 +0530

    Update downgrade.sh

--- downgrade.sh
+++ downgrade.sh
@@ -2,7 +2,7 @@
 
 # Iterate over files in the directory
 for f in ~/gallery-dl/*; do
-   # Check if the file is an image file
+   
    if [[ -f "$f" && ( "$f" == *.jpg || "$f" == *.jpeg || "$f" == *.png || "$f" == *.gif  || "$f" == *.mp4 ) ]]; then
       convert "$f" -quality 70% "$f"
       echo "Downgraded $f"
