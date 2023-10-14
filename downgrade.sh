0266dce9a033b25ca7981839f866990be4462e5a
Flynn Sharwood <69000191+flynnsharwood@users.noreply.github.com>
2023-10-14 17:24:43 +0530

    Update downgrade.sh

--- downgrade.sh
+++ downgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Iterate over files in the directory
-for f in ~/gallery-dl/redgifs/*; do
+for f in ~/gallery-dl/*; do
    # Check if the file is an image file
    if [[ -f "$f" && ( "$f" == *.jpg || "$f" == *.jpeg || "$f" == *.png || "$f" == *.gif  || "$f" == *.mp4 ) ]]; then
       convert "$f" -quality 70% "$f"
